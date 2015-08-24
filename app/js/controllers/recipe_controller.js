0675fd22cd0ca6027cf991871827eb93bde4aa57
Nick Kuhn <nickuhn@gmail.comn>
2015-08-24 11:33:53 -0700

    working api call

--- app/js/controllers/recipe_controller.js
+++ app/js/controllers/recipe_controller.js
@@ -1,10 +1,27 @@
-'use stirct';
+'use strict';
 
 module.exports = function(app) {
-	app.controller('recipeController', ['$scope', function($scope) {
+	app.controller('recipeController', ['$scope', '$http', function($scope, $http) {
 		$scope.recipes = [];
-		$scope.erros = [];
+		$scope.errors = [];
 
-		//CRUD GOES HERE
+    $scope.getRecipes = function(ingredients) {
+      var APIKEY = 'b42730b75912919136b1fcc58fec5b5c'
+      var ingredientString = '';
+      for (var prop in ingredients) {
+        var temp = ingredients[prop].replace(' ', '%20');
+        ingredientString = ingredientString + ',' + temp;
+      }
+      var url = 'http://food2fork.com/api/search?key=' + APIKEY + '&q=' + ingredientString;
+      $http.get(url)
+        .then(function(res) {
+          console.log('success', res);
+          $scope.recipes = res.data.recipes;
+        },
+        function(res) {
+          console.log('error', res);
+          $scope.errors.push(res);
+        })
+    }
 	}]);
 };
