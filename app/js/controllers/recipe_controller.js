978a8f0bdd3a3c4b17d341f931fe79958ae7c98c
Conlan McNamee <conlanmcnamee@gmail.com>
2015-08-25 09:57:01 -0700

    added outgredients and start of sorting

--- app/js/controllers/recipe_controller.js
+++ app/js/controllers/recipe_controller.js
@@ -4,16 +4,21 @@ module.exports = function(app) {
 	app.controller('recipeController', ['$scope', '$http', function($scope, $http) {
 		$scope.recipes = [];
 		$scope.errors = [];
+    $scope.sortType = 'recipeName';
 
-    $scope.getRecipes = function(ingredients) {
+    $scope.getRecipes = function(ingredients, outgredients) {
       var ingredientString = '';
-      var APIKEY = 'd148c8da9c3ac06a293e1300968ee4b3'
-      var ingredientString = '';
+      var outgredientString = '';
+      var APIKEY = 'd148c8da9c3ac06a293e1300968ee4b3';
       for (var prop in ingredients) {
         var temp = ingredients[prop].replace(' ', '%20');
-        ingredientString = ingredientString + ',' + temp;
+        ingredientString = ingredientString + '&allowedIngredient[]=' + temp;
       }
-      var url = 'http://api.yummly.com/v1/api/recipes?_app_id=ca33a09c&_app_key=458d12f8aa1a7682b4f947c7375a93dd&q=' + ingredientString;
+      for (var prop in outgredients) {
+        var temp = outgredients[prop].replace(' ', '%20');
+        outgredientString = outgredientString + '&excludedIngredient[]=' + temp;
+      }
+      var url = 'http://api.yummly.com/v1/api/recipes?_app_id=ca33a09c&_app_key=458d12f8aa1a7682b4f947c7375a93dd&q=' + ingredientString + outgredientString;
       $http.get(url)
         .then(function(res) {
           console.log('success', res);
