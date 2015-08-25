67a1023d7c91e657ee2c194aaf753521cdf395c6
Nick Kuhn <nickuhn@gmail.comn>
2015-08-25 15:48:32 -0700

    added star rating

--- app/client.js
+++ app/client.js
@@ -5,5 +5,6 @@ require('angular/angular');
 var recipeApp = angular.module('recipeApp', []);
 
 require('./js/directives/recipe_directive.js')(recipeApp);
+require('./js/directives/rating_directive.js')(recipeApp);
 require('./js/controllers/recipe_controller.js')(recipeApp);
 require('./js/controllers/ingredient_controller.js')(recipeApp);
