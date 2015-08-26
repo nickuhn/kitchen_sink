3d664783536f138b718ebd48081e538d9644ee09
Nick Kuhn <nickuhn@gmail.comn>
2015-08-26 16:37:20 -0700

    https

--- app/js/controllers/recipe_controller.js
+++ app/js/controllers/recipe_controller.js
@@ -77,7 +77,7 @@ module.exports = function(app) {
         }
       }
       outgredientString += '&requirePictures=true'
-      var url = 'http://api.yummly.com/v1/api/recipes?_app_id=ca33a09c&_app_key=458d12f8aa1a7682b4f947c7375a93dd&q=' + ingredientString + outgredientString + pageChange();
+      var url = 'https://api.yummly.com/v1/api/recipes?_app_id=ca33a09c&_app_key=458d12f8aa1a7682b4f947c7375a93dd&q=' + ingredientString + outgredientString + pageChange();
 
       console.log(url);
 
